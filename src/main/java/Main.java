b76413faf9d57f222922634a8169bc8530b34bce
MuhammadSiddiqui121 <Musiddiqui@s.sfusd.edu>
2025-01-27 09:31:55 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,4 +10,24 @@ public class Main {
 
 
 }
+public void setup() 
+{
+  String[] lines = {"beast","dough","happy","question","star","three","eagle","try"};
+  System.out.println("there are " + lines.length + " lines");
+  for (int i = 0 ; i < lines.length; i++) 
+  {
+    System.out.println(pigLatin(lines[i]));
+  }
+}
+public void draw()
+{
+        //not used
+}
+public int findFirstVowel(String sWord){
+  for(int i = 0; i < sWord.length(); i++)
+       if(sWord.substring(i,i+1).equals("a")||sWord.substring(i,i+1).equals("e")||sWord.substring(i,i+1).equals("i")||sWord.substring(i,i+1).equals("o")||sWord.substring(i,i+1).equals("u"))
+      return i;
+  return -1;
+}
+
 
